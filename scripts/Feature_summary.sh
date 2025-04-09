2b36b96c40b74b6e7c4376e971d8447cfb0f8588
shasanka2710 <shasanka.learning@gmail.com>
2025-04-09 23:33:47 +0530

    added demo

--- scripts/Feature_summary.sh
+++ scripts/Feature_summary.sh
@@ -7,6 +7,7 @@ response=$(curl -s -u "$JIRA_EMAIL:$JIRA_API_TOKEN" \
 "https://$JIRA_DOMAIN/rest/agile/1.0/board/$BOARD_ID/issue?jql=issuetype=Story%20AND%20fixVersion=%22$FIX_VERSION%22%20AND%20'Feature%20Link'%20is%20not%20EMPTY&fields=customfield_10091,status")
 echo "Response from Jira API: $response"
 
+# Check if the response contains any issues
 # Parse the response to extract stories
 echo "Parsing stories..."
 stories=$(echo "$response" | jq -r '.issues[] | "\(.fields.customfield_10091)=\(.fields.status.name)"')
