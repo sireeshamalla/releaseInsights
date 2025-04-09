9d5de15ea8fdef410bf960533a3f27e39b22c1ae
shasanka2710 <shasanka.learning@gmail.com>
2025-04-09 22:49:03 +0530

    added demo

--- scripts/Feature_summary.sh
+++ scripts/Feature_summary.sh
@@ -2,7 +2,7 @@
 
 # Fetch stories with Feature Link, description, and status
 response=$(curl -s -u "$JIRA_EMAIL:$JIRA_API_TOKEN" \
-"https://$JIRA_DOMAIN/rest/agile/1.0/board/$BOARD_ID/issue?jql=issuetype=Story%20AND%20fixVersion=%22$FIX_VERSION%22%20AND%20'Feature%20Link'%20is%20not%20EMPTY&fields=customfield_10091,status"
+"https://$JIRA_DOMAIN/rest/agile/1.0/board/$BOARD_ID/issue?jql=issuetype=Story%20AND%20fixVersion=%22$FIX_VERSION%22%20AND%20'Feature%20Link'%20is%20not%20EMPTY&fields=customfield_10091,status")
 # Parse the response to extract stories
 stories=$(echo "$response" | jq -r '.issues[] | "\(.fields.customfield_10091)=\(.fields.status.name)"')
 
