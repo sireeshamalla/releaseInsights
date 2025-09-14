9f6270589a7bd944dd16c4fb9c8f85a559691b60
sireesha <sireesha.malla@gmail.com>
2025-09-14 09:25:24 -0700

    sonar corrections

--- scripts/fetch_sonarcloud_metrics.sh
+++ scripts/fetch_sonarcloud_metrics.sh
@@ -8,6 +8,7 @@ echo "SONAR_PROJECT_KEY: $SONAR_PROJECT_KEY"
 echo "SONAR_HOST_URL: $SONAR_HOST_URL"
 echo "METRICS: $METRICS"
 echo "SONAR_BRANCH: $SONAR_BRANCH"
+echo "calling sonar API to fetch metrics..."
 response=$(curl -s -u $SONAR_TOKEN: "$SONAR_HOST_URL/api/measures/component?component=$SONAR_PROJECT_KEY&metricKeys=$METRICS&branch=$SONAR_BRANCH")
 echo "SonarCloud API response: $response"
 coverage=$(echo $response | jq -r '.component.measures[] | select(.metric=="coverage") | .value')
