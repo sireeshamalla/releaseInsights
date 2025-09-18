8a39abc6c8cb38f4ad504b45afa9b927adc750cc
sireesha <sireesha.malla@gmail.com>
2025-09-18 08:52:03 +0530

    karate corrections

--- scripts/Feature_summary.sh
+++ scripts/Feature_summary.sh
@@ -109,7 +109,7 @@ style_block="<style>
 # Display the results
 echo "Generating HTML table..."
 html_table="<table style='border-collapse:collapse; width:100%;'>"
-html_table="${html_table}<tr><th style='border:1px solid #000; background-color:#e3f2fd;'>Feature</th><th style='border:1px solid #000; background-color:#e3f2fd;'>Summary</th><th style='border:1px solid #000; background-color:#e3f2fd;'>Completion Percentage</th><th style='border:1px solid #000; background-color:#e3f2fd;'>TestCase Ids</th></tr>"
+html_table="${html_table}<tr><th style='border:1px solid #000; background-color:#e3f2fd;'>Feature</th><th style='border:1px solid #000; background-color:#e3f2fd;'>Summary</th><th style='border:1px solid #000; background-color:#e3f2fd;'>Completion Percentage</th></tr>"
 for feature_url in "${!feature_map[@]}"; do
   total_stories=${feature_map["$feature_url"]}
   done_stories=${feature_status_count["$feature_url"]}
@@ -119,7 +119,7 @@ for feature_url in "${!feature_map[@]}"; do
   testcaseids=$(echo "${feature_testcaseids_map["$feature_url"]}" | sed 's/^,//')
   # Remove leading @ from each TestCaseId
   clean_testcaseids=$(echo "$testcaseids" | sed 's/@//g')
-  html_table="${html_table}<tr style='border:1px solid #000;'><td style='border:1px solid #000;'>${feature_url}</td><td style='border:1px solid #000;'>${feature_summary}</td><td style='border:1px solid #000;'>${completion_percentage}%</td><td style='border:1px solid #000;'>${clean_testcaseids}</td></tr>"
+  html_table="${html_table}<tr style='border:1px solid #000;'><td style='border:1px solid #000;'>${feature_url}</td><td style='border:1px solid #000;'>${feature_summary}</td><td style='border:1px solid #000;'>${completion_percentage}%</td></tr>"
 done
 html_table="${html_table}</table>"
 
